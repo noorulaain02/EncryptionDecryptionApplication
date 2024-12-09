dc119ac7b6d3a0ff06cdb81f2ac57b83e77a1b1d
unknown <noorulaain34ghazi@gmail.com>
2024-12-10 00:42:34 +0500

    Initial commit

---
+++ EncryptDecrypt/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from EncryptDecrypt.models import Image,Send,Receive
+
+admin.site.register(Image)
+admin.site.register(Send)
+admin.site.register(Receive)
+# Register your models here.
